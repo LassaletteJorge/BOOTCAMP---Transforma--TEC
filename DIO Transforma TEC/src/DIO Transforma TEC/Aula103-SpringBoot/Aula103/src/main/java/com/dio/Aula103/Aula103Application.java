c3b5077e445fa3d4bed2208958d50f0f918dd2d0
LassaletteJorge <vlmajorge2@gmail.com>
2022-08-24 16:46:51 -0300

    Novo Commmit

---
+++ DIO Transforma TEC/src/DIO Transforma TEC/Aula103-SpringBoot/Aula103/src/main/java/com/dio/Aula103/Aula103Application.java
@@ -0,0 +1,13 @@
+package com.dio.Aula103;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aula103Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aula103Application.class, args);
+	}
+
+}
